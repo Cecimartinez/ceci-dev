08e67af6d377126ebb1045f34feff40e4577fe5c
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-11 17:50:21 -0300

    Data Services

---
+++ astro-blog/src/data/services.ts
@@ -0,0 +1,17 @@
+export const services = [
+  {
+    name: "UX/UI Design",
+    description: "Enhance your online presence with intuitive and visually appealing interfaces that captivate and engage your audience.",
+    imgSrc: "././chef-hat.svg",
+  },
+  {
+    name: "Web Development",
+    description: "Transform your vision into reality with engaging web solutions that connect you with your audience and drive results.",
+    imgSrc: "././chef-hat.svg",
+  },
+  {
+    name: "Website Design",
+    description: "Elevate your online presence with stunning, user-centric website designs that captivate your audience and boost your brand.",
+    imgSrc: "././chef-hat.svg",
+  }
+]
