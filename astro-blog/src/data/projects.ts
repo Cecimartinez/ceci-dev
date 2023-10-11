ded6c872c96a10913f2e1fbf9c11c0d2135be424
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-10-11 11:28:58 -0300

    refactoring of the projects section

---
+++ astro-blog/src/data/projects.ts
@@ -0,0 +1,73 @@
+export const projects = [
+  {
+    name: "Restaurant Foodie",
+    description: "Landing page - Foodie restaurant",
+    imgSrc: "././chef-hat.svg",
+    link: "https://cecimartinez.github.io/restaurant-foodie/",
+
+  },
+  {
+    name: "Portfolio",
+    description: "My personal portfolio website",
+    imgSrc: "././user-scan.svg",
+    link: "https://cecimartinez.github.io/Portafolio/",
+
+  },
+  {
+    name: "People Table",
+    description:
+      "Table of employee data according to their category and company",
+    imgSrc: "././users.svg",
+    link: "https://github.com/Cecimartinez/GP_TEST",
+
+  },
+  {
+    name: "Delicate Macarons",
+    description: "A website for macarons lovers",
+    imgSrc: "././cookie.svg",
+    link: "https://cecimartinez.github.io/Delicate-Macarons/",
+
+  },
+  {
+    name: "Whether App",
+    description: "A weather forecast application",
+    imgSrc: "././cloud-filled.svg",
+    link: "https://github.com/Cecimartinez/Weather-app",
+
+  },
+  {
+    name: "Medical Center",
+    description: "Medical Center Patient Management Application",
+    imgSrc: "././building-hospital.svg",
+    link: "https://github.com/Cecimartinez/centro-medico",
+
+  },
+  {
+    name: "Counter App",
+    description: "A simple counter application",
+    imgSrc: "././123.svg",
+    link: "https://github.com/Cecimartinez/Counter-App",
+
+  },
+  {
+    name: "Calculator",
+    description: "A basic calculator tool",
+    imgSrc: "././calculator-filled.svg",
+    link: "https://cecimartinez.github.io/Calculadora/",
+
+  },
+  {
+    name: "Four in Line Game",
+    description: "A classic four-in-a-row game",
+    imgSrc: "././dice-4.svg",
+    link: "https://github.com/Cecimartinez/juego-cuatro-en-linea",
+
+  },
+  {
+    name: "Challenge Dev - NCR",
+    description: "A development challenge project",
+    imgSrc: "././credit-card.svg",
+    link: "https://github.com/Cecimartinez/TestChallengeDev_NCR",
+
+  },
+];
